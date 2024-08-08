5426922d5633ce08fb2b864288ac6c89f84f4cc7
Sther-asr <salazar-sther@hotmail.com>
2024-08-07 22:04:54 -0500

    actualizaciones 2024

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+const primaryNav = document.querySelector('.nav-ul');
+const navToggle = document.querySelector('.toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if(visibility === "false"){
+        primaryNav.setAttribute('data-visible', true);
+        primaryNav.setAttribute('aria-expanded', true);
+        navToggle.setAttribute('src', "/Img/close-hamburguesa.png");
+    }else {
+        primaryNav.setAttribute('data-visible', false);
+        primaryNav.setAttribute('aria-expanded', false);
+        navToggle.setAttribute('src', "/Img/Hamburguesita.png");
+    }
+})
